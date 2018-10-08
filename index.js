08189a38e43a7ec2807c6b09a1c2081b53473fdf
deraviyam <deraviyam@gmail.com>
2018-10-08 19:58:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World!');
+  //res.send('!Hello World!');
+  res.sendfile('homepage.html');
 });
 
 var server = app.listen(3000, function () {
